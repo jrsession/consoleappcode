223f094a3b210f8f35eb3fd47940b1be0650b7a7
jr session <jrsession@gmail.com>
2024-03-18 13:58:32 -0700

    Add project files.

---
+++ Domain/Entities/Subscribers.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class Subscribers
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public bool isSubscribed { get; set; }
+    }
+}
---
+++ Domain/Interfaces/ISubscriberRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface ISubscriberRepository
+    {
+        Task<IEnumerable<Subscribers>> GetAllSubscribersAsync();
+        Task<Subscribers> Addsubscribers(Subscribers subscriber);
+        Task<Subscribers> RemoveSubscriber(Guid subscriber);
+    }
+}
